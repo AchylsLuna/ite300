a2cf3070de686d64c993acde1fa024c9b8ddf742
Achyls Luna <jomasenriquez2004@gmail.com>
2024-09-04 06:46:19 +0800

    Deleted unnecessary file from the branch

--- .vscode/space.py/counter.py
+++
@@ -1,9 +0,0 @@
-import random
-from collections import Counter
-obj_list = ['a', 'b', 'c', 'd']
-
-counter = Counter()
-for _ in range(100):
-        mum_list = random.choice(obj_list)
-        counter[mum_list] += 1
-        print(counter)
--- sample.py
+++
@@ -1,4 +0,0 @@
-threedimensional = [[[1, 2, 3, 4,]]]
-twodimensional = [[2,3,5,1]]
-
-print(threedimensional)
